a0d14a70bc23f651e199e4c1bfe301c08009d1bb
jichangjun <jinsdu@outlook.com>
2022-01-23 13:52:55 +0800

    feat: support daemonset

--- pkg/controller/namespace.go
+++ pkg/controller/namespace.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	apps "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/strategicpatch"
@@ -120,5 +121,123 @@ func (c *controller) patchStatefulsetWithAnnotation(s *apps.StatefulSet, annotat
 		klog.V(2).InfoS("failed to patch annotation for statefulset", "statefulset", s.Name, "err", err)
 		return nil, err
 	}
+	return result, nil
+}
+
+// hardcode flag for sleep daemonset
+var defaultExpression = v1.NodeSelectorRequirement{
+	Key:      "sleep.kubefree.com/not-existing-key",
+	Operator: v1.NodeSelectorOpExists,
+	Values:   []string{"sleep"},
+}
+
+func (c *controller) patchDaemonsetWithNodeAffinity(s *apps.DaemonSet, annotation, value string) (*apps.DaemonSet, error) {
+	if s == nil {
+		return nil, fmt.Errorf("unexpected statefulset as nil")
+	}
+
+	oldData, err := json.Marshal(s)
+	if err != nil {
+		return nil, err
+	}
+
+	newD := s.DeepCopy()
+	if newD.Annotations == nil {
+		newD.Annotations = make(map[string]string)
+	}
+
+	// still patch daemonset with legacy-replicas annotation
+	newD.Annotations[annotation] = value
+
+	if newD.Spec.Template.Spec.Affinity == nil {
+		newD.Spec.Template.Spec.Affinity = &v1.Affinity{}
+	}
+
+	if newD.Spec.Template.Spec.Affinity.NodeAffinity == nil {
+		newD.Spec.Template.Spec.Affinity.NodeAffinity = &v1.NodeAffinity{}
+	}
+
+	if newD.Spec.Template.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution == nil {
+		newD.Spec.Template.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution = &v1.NodeSelector{}
+	}
+
+	if newD.Spec.Template.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution.NodeSelectorTerms == nil {
+		newD.Spec.Template.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution.NodeSelectorTerms = []v1.NodeSelectorTerm{}
+	}
+
+	selectorTerm := v1.NodeSelectorTerm{}
+	selectorTerm.MatchExpressions = append(selectorTerm.MatchExpressions, defaultExpression)
+	newD.Spec.Template.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution.NodeSelectorTerms = append(newD.Spec.Template.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution.NodeSelectorTerms, selectorTerm)
+
+	newData, err := json.Marshal(newD)
+	if err != nil {
+		return nil, err
+	}
+
+	patchBytes, err := strategicpatch.CreateTwoWayMergePatch(oldData, newData, &apps.DaemonSet{})
+	if err != nil {
+		return nil, err
+	}
+
+	result, err := c.clientset.AppsV1().DaemonSets(s.Namespace).Patch(context.TODO(), s.Name, types.StrategicMergePatchType, patchBytes, metav1.PatchOptions{})
+	if err != nil {
+		klog.V(2).InfoS("failed to patch annotation for daemonsets", "daemonsets", s.Name, "err", err)
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (c *controller) patchDaemonsetWithoutSpecificNodeAffinity(s *apps.DaemonSet, key, value string) (*apps.DaemonSet, error) {
+	if s == nil {
+		return nil, fmt.Errorf("unexpected statefulset as nil")
+	}
+
+	oldData, err := json.Marshal(s)
+	if err != nil {
+		return nil, err
+	}
+
+	newD := s.DeepCopy()
+	if newD.Spec.Template.Spec.Affinity == nil ||
+		newD.Spec.Template.Spec.Affinity.NodeAffinity == nil ||
+		newD.Spec.Template.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution == nil ||
+		newD.Spec.Template.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution.NodeSelectorTerms == nil ||
+		len(newD.Spec.Template.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution.NodeSelectorTerms) == 0 {
+
+		return nil, nil
+	}
+
+	var newSelectorTerm = []v1.NodeSelectorTerm{}
+	for _, st := range newD.Spec.Template.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution.NodeSelectorTerms {
+		var newExpression = []v1.NodeSelectorRequirement{}
+		for _, express := range st.MatchExpressions {
+			// remove default expression patched by kubefree
+			if express.Key != defaultExpression.Key {
+				newExpression = append(newExpression, express)
+			}
+		}
+		var newNodeSelectorTerm = st
+		newNodeSelectorTerm.MatchExpressions = newExpression
+		newSelectorTerm = append(newSelectorTerm, newNodeSelectorTerm)
+	}
+	newD.Spec.Template.Spec.Affinity.NodeAffinity.RequiredDuringSchedulingIgnoredDuringExecution.NodeSelectorTerms = newSelectorTerm
+
+	newData, err := json.Marshal(newD)
+	if err != nil {
+		return nil, err
+	}
+
+	patchBytes, err := strategicpatch.CreateTwoWayMergePatch(oldData, newData, &apps.DaemonSet{})
+	if err != nil {
+		return nil, err
+	}
+
+	result, err := c.clientset.AppsV1().DaemonSets(s.Namespace).Patch(context.TODO(), s.Name, types.StrategicMergePatchType, patchBytes, metav1.PatchOptions{})
+	if err != nil {
+		klog.V(2).InfoS("failed to patch annotation for daemonsets", "daemonsets", s.Name, "err", err)
+		return nil, err
+	}
+
 	return result, nil
 }
